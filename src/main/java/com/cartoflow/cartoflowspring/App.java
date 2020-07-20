97c145ba8111aed3111a698d8f752ca24580d8f4
BAOUN <ahmed-nadir.baoun@capgemini.com>
2020-07-20 16:14:09 +0200

    Fixed new version

--- src/main/java/com/cartoflow/cartoflowspring/App.java
+++ src/main/java/com/cartoflow/cartoflowspring/App.java
@@ -1,20 +1,47 @@
 package com.cartoflow.cartoflowspring;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.cartoflow.cartoflowspring"})
+@SpringBootApplication
+public class App implements CommandLineRunner {
 
-public class App implements CommandLineRunner{
-  
+  @Autowired
+  private UserRepository repository;
+
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
   }
 
-@Override
+  @Override
   public void run(String... args) throws Exception {
-    System.out.println("Application is running");
-  }
-}
 
+    repository.deleteAll();
+
+    // save a couple of Users
+    repository.save(new User("Alice", "Smith"));
+    repository.save(new User("Bob", "Smith"));
+
+    // fetch all Users
+    System.out.println("Users found with findAll():");
+    System.out.println("-------------------------------");
+    for (User User : repository.findAll()) {
+      System.out.println(User);
+    }
+    System.out.println();
+
+    // fetch an individual User
+    System.out.println("User found with findByFirstName('Alice'):");
+    System.out.println("--------------------------------");
+    System.out.println(repository.findUserByFirstName("Alice"));
+
+    System.out.println("Users found with findByLastName('Smith'):");
+    System.out.println("--------------------------------");
+    for (User User : repository.findUserByLastName("Smith")) {
+      System.out.println(User);
+    }
+
+  }
+
+}
--- src/main/java/com/cartoflow/cartoflowspring/HelloRessource.java
+++ src/main/java/com/cartoflow/cartoflowspring/HelloRessource.java
@@ -2,7 +2,6 @@ package com.cartoflow.cartoflowspring;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
